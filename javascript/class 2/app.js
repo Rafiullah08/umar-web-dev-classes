997548e67b23fa66da5fc216e4a19506e7510256
Rafi ullah <you@example.com  git config --global user.name Your>
2025-04-21 00:14:44 +0500

    files add

---
+++ javascript/class 2/app.js
@@ -0,0 +1,57 @@
+// alert("welcome")
+
+// variables
+
+// var num = 12
+
+// console.log(num);
+
+// string
+// set of alphabets and numbers
+
+// var std = "6887"
+
+// var score = false
+
+// var std2 ;
+
+// console.log(typeof std2);
+
+
+
+
+
+// string 
+// numbers
+// bholeen
+// undefined
+// null
+
+var student;
+
+var num ;       // variable declare
+
+num = 12
+// console.log(num );
+
+
+var num2 = 10
+
+// variable reassinged 
+
+
+// var age = 23
+
+//  age = 25
+
+// console.log(age);
+
+
+var std = "umar"
+
+std = "rafi"
+
+console.log(std);
+
+var age = 22
+age = 25
---
+++ javascript/class 3/app.js
@@ -0,0 +1,72 @@
+// alert("hello")
+
+// var num = 23
+
+// let num2 = 34
+
+// const num3 = 45
+
+// // 2 version
+
+// // 1995 to 2015 => var
+
+
+
+
+// // achma script6
+
+// // 2015 to 2025 => let const
+
+
+
+// var name = "rafi"
+
+// var name2;
+
+
+// name2 = "sami"
+
+
+
+// var std = "aruba"
+// std = "umar"
+// console.log("std", std );
+
+
+// let age = 23
+// let age2;
+
+// let std3 = "sami"
+
+// std3 = "faizan"
+
+// console.log("std3", std3);
+
+var name; 
+
+
+
+var std = "sami"
+
+
+
+var std = "umar"
+
+console.log(std);
+
+let age;
+
+let std2 = "aruba"
+
+let std2 = "same"
+
+console.log(std2);
+
+const name = "rafi"
+
+// const age;
+
+const mystd = "same"
+const mystd = "navid"
+
+console.log(name);
