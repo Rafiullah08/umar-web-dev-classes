6fd20ef6c878338b473aa12c5104764768c73dbd
Rafi ullah <you@example.com  git config --global user.name Your>
2025-05-17 22:38:04 +0500

    chp 13 completed

---
+++ javascript/class 7/app.js
@@ -0,0 +1,73 @@
+// alert("hello")
+
+// prompt
+
+// let name = prompt("enter your name")
+
+// let num1 = prompt("enter num1")
+// let num2 = prompt("enter num2")
+
+// let add = num1 * num2
+
+// alert(add , "Result")
+
+// let age2 = window.prompt("new")
+
+// if else 
+// let std = "female" 
+
+// if(std == "male"){
+//     console.log("allowed");
+    
+// } else {
+//     console.log("not allowed");
+    
+// }
+
+// let age = 10
+
+// if(age < 20){
+//     alert("allowed")
+// } else {
+//     alert("not allowed")
+// }
+
+
+// = => assinging operator
+// == => equal sign / check value
+// === => comparision operator  /  chek value and data type
+
+
+// let num2 = 20
+
+
+
+// if(num2 !== "2"){
+//     console.log("good");
+    
+// } else{
+//     console.log(" not good");
+
+// }
+
+
+
+let city = "peshawddder"
+
+if(city == "karachi"){
+    console.log("the city of light");
+    
+} else if(city == "peshawer"){
+    console.log("the city of grmiii");
+    
+} else{
+    console.log("kuch bhi nhi");
+    
+}
+
+
+
+
+
+
+
---
+++ javascript/class 8/app.js
@@ -0,0 +1,57 @@
+// alert("hello")
+
+
+
+// let city = "karachi"
+
+
+// let userCity = prompt("enter your city")
+
+// if(userCity == city){
+//     document.writeln("welcome to the city of light")
+// } else {
+//     document.writeln("please enter a valid city")
+    
+// }
+
+
+// let userCity = prompt("enter your city name")
+
+
+// if(userCity == "karachi"){
+//     document.writeln("welcome to the city of light")
+// } else if(userCity == "peshawer"){
+//     document.writeln("welcome to the city of charsi")
+
+// } else if(userCity == "lahore"){
+//     document.writeln("welcome to the city of punjabi")
+
+// } else {
+//     document.writeln("enter a valid city")
+
+// }
+
+// gates
+// && , ||
+
+
+
+
+let age = 10
+
+// if(age > 20 && age < 42){
+//     console.log("allowed");
+    
+// } else {
+//     console.log("not allowed");
+
+// }
+
+
+if(age > 15 || age < 30) {
+    console.log("allowed student");
+    
+} else {
+    console.log("not allowed std");
+    
+}
