27c681a0cb562648c7e222c349c800e2b1323f1e
Rafi ullah <you@example.com  git config --global user.name Your>
2025-05-25 23:29:56 +0500

    array start

---
+++ javascript/class 9/app.js
@@ -0,0 +1,75 @@
+// alert("hello")
+
+
+let std = "male"
+
+// if(std === "male"){
+//     console.log("enter into class");
+    
+// } else {
+//     console.log(" not enter into class");
+
+// }
+
+
+// nested condition
+
+// let age = 19
+// let gender = "male"
+
+
+// if(age > 20){
+
+//     if(gender === "male"){
+// alert("allow ")
+//     } else {
+//         alert("not allow")
+//     }
+
+// } else{
+//     alert("invalid student")
+// }
+
+
+
+// normal variables
+
+// var 
+// let 
+// const
+
+// special variables
+
+// arrays
+
+
+
+
+
+let city1 = "karachi"
+
+let city2 = "lahore"
+let city3 = "peshawer"
+let age = 12
+
+
+let array = ["karachi", "lahore", "peshawer",1,2,3,4,5,4]
+
+
+console.log("arrays", array);
+
+
+
+// index number => 0,1,2,3,4,
+// array length => 1,2,3
+
+
+
+
+
+
+
+
+
+
+
