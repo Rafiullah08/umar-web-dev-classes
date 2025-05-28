a51411cb87e982d5289b46bcb047db96513d3500
Rafi ullah <you@example.com  git config --global user.name Your>
2025-05-28 23:13:53 +0500

    for loop start

---
+++ javascript/class 11/app.js
@@ -0,0 +1,42 @@
+// alert("hello")
+
+
+// document.writeln("hello world", "<br>")
+// document.writeln("hello world", "<br>")
+// document.writeln("hello world", "<br>")
+// document.writeln("hello world", "<br>")
+// document.writeln("hello world", "<br>")
+// document.writeln("hello world")
+// document.writeln("hello world")
+// document.writeln("hello world")
+// document.writeln("hello world")
+// document.writeln("hello world")
+// document.writeln("hello world")
+// document.writeln("hello world")
+// document.writeln("hello world")
+
+// 0,1,2,3,9,
+
+// for(var i = 10; i < 100; i++){
+//     document.writeln(i,"<br>")
+// }
+
+
+
+// var j = 10
+
+// for(j;j<= 100; j++){
+//     document.writeln("hello rafi",j,"<br>")
+
+// }
+
+
+
+
+// 5 * 1 = 5
+
+let tableNum = 12
+
+for(var i = 1; i <= 60; i++){
+    document.writeln(tableNum+ " "+  "x"+ " "+ i + "=" + tableNum * i ,"<br>")
+}
