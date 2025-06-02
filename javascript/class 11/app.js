3bf8e2adbeecd90e915d4d62749dbe5463e7c3ee
Rafi ullah <you@example.com  git config --global user.name Your>
2025-06-02 22:47:14 +0500

    flag concept complete

--- javascript/class 11/app.js
+++ javascript/class 11/app.js
@@ -35,8 +35,26 @@
 
 // 5 * 1 = 5
 
-let tableNum = 12
+// let tableNum = 12
 
-for(var i = 1; i <= 60; i++){
-    document.writeln(tableNum+ " "+  "x"+ " "+ i + "=" + tableNum * i ,"<br>")
+// for(var i = 1; i <= 60; i++){
+//     document.writeln(tableNum+ " "+  "x"+ " "+ i + "=" + tableNum * i ,"<br>")
+// }
+
+
+let array = ["karachi", "lahore", "chitral"]
+
+for(var i = 0 ;i < array.length; i++){
+    // console.log(array[i]);
+    var ismatch = false
+    if(array[i]=== "chitraljjj"){
+        console.log("welcome to karachi")
+        ismatch = true
+      break
+   
+    } 
+}
+
+if(ismatch === false){
+    alert("karachi not exits")
 }
---
+++ javascript/class 12/app.js
@@ -0,0 +1,38 @@
+// alert("hello")
+
+// falg concept
+
+
+
+// let array = ["karachi", "lahore", "chitral", "peshawer"]
+
+
+
+// var ismatch = false
+
+// for(var i = 0; i < array.length; i++){
+//     // console.log(array[i]);
+
+// if(array[i] === "karachi"){
+
+// ismatch = true
+// alert("welcome to karachi")
+// } 
+   
+// }
+
+// if(ismatch === false){
+//     alert("this city is not exits")
+// }
+
+
+
+
+for(var i = 1;i< 10 ;i++){
+    document.writeln(i + "<br>")
+
+if(i == 5){
+    break
+}
+
+}
