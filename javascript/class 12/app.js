a67706b4c7ff27e53cfe73dab8e52e520c2eb966
Rafi ullah <you@example.com  git config --global user.name Your>
2025-06-04 22:58:41 +0500

    completed nested loop and changing case

--- javascript/class 12/app.js
+++ javascript/class 12/app.js
@@ -28,11 +28,38 @@
 
 
 
-for(var i = 1;i< 10 ;i++){
-    document.writeln(i + "<br>")
+// for(var i = 1;i< 10 ;i++){
+//     document.writeln(i + "<br>")
 
-if(i == 5){
-    break
-}
+// if(i == 5){
+//     break
+// }
 
-}
+// }
+
+
+
+// for(var a = 1; a <= 100 ; a = a+10){
+//     for(var b = a; b < a+10 ; b++){
+//         document.writeln(b)
+//     }
+//         document.writeln("<br>")
+    
+// }
+
+
+// for(var a =1 ; a<= 60 ; a++){
+//     for(var b = 1; b < a; b++){
+//         document.writeln(b)
+//     }
+//     document.writeln("<br>")
+// }
+
+
+
+// for(var a = 1; a<= 100 ;  a++){
+//     for(var b = 1; b < a; b++){
+//         document.writeln(b)
+//     }
+//     document.writeln("<br>")
+// }
---
+++ javascript/class 13/app.js
@@ -0,0 +1,42 @@
+// alert("hello")
+
+// nested loop
+
+// 
+// outer loop , inner loop
+
+
+// for(var a = 1; a<= 100; a = a + 10){ 
+// for(var b = a; b < a+10; b++){
+//     document.writeln(b)
+// }
+// document.writeln("<br>")
+// }
+
+
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+// for(var a =1 ; a <= 60; a++ ){
+
+//  for(var b = 1; b < a; b++){
+//     document.writeln(b)
+//  }
+//  document.writeln("<br>")
+// }
+
+
+
+// var std  = "rafi"
+
+// document.writeln(std.toUpperCase())
+
+// var std2 = "RAFIULLAH"
+// document.writeln(std2.toLocaleLowerCase())
+
+let userName  = prompt("enter your name")
+
+document.writeln(userName.toLocaleLowerCase())
