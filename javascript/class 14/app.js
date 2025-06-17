6f4b8ed3c58fc858c4a3725ca151e11e4c8356de
Rafi ullah <you@example.com  git config --global user.name Your>
2025-06-17 22:44:44 +0500

    Rounding numbers complete

---
+++ javascript/class 14/app.js
@@ -0,0 +1,48 @@
+let myName = "my name is rafiullah and rafiullah"
+
+// let newVar = myName.charAt().toLocaleUpperCase()
+
+// console.log(newVar);
+
+
+// let index = myName.indexOf("rafiullah")
+
+// console.log(index);
+
+// let rpl = myName.replace("rafiullah", "umar")
+
+// global replace
+// let rpl = myName.replace(/rafiullah/g, "umar")
+
+// console.log(myName);
+// console.log(rpl);
+
+
+let age = 23  //=> inte
+let price = 23.5566 // => decimal
+
+// console.log(typeof(age));
+// console.log(typeof(price));
+
+
+let newNum = 6.7555
+let round = Math.round(newNum)
+
+
+let newNum2 = 23.767600078889
+let ciel = Math.ceil(newNum2)
+
+
+
+let newNum3 = 54.4555566
+let floor = Math.floor(newNum3)
+
+console.log(newNum3);
+console.log(floor);
+
+
+
+
+
+
+
