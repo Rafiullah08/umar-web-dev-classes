cc629267068239d0303e80e75e35835c5af4d180
Rafi ullah <you@example.com  git config --global user.name Your>
2025-06-18 23:27:37 +0500

    Conversion between string and number completed, including the use of the toFixed() method.

---
+++ javascript/class 15/app.js
@@ -0,0 +1,27 @@
+// alert("hello")
+
+
+// Math.random
+
+// let randomNum =Math.floor(Math.random()* 1000 + 100) 
+// console.log(randomNum);
+
+let myName = "rafiullah"
+
+let num = Number(myName)
+
+
+// console.log(typeof myName);
+// console.log(typeof num);
+
+// let num1 = 55656
+
+// let str = num1.toString()
+
+// console.log(typeof str);
+
+let decimalNum = 34.5556666
+
+console.log(decimalNum.toFixed(1));
+
+
