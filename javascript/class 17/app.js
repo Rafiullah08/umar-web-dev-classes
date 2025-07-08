9b56ee440df3de9d37861ca2f6f0c3ad99c9a26b
Rafi ullah <you@example.com  git config --global user.name Your>
2025-07-08 22:23:46 +0500

    function and Passing them data

---
+++ javascript/class 17/app.js
@@ -0,0 +1,41 @@
+// alert("Hello, World!");
+
+// function
+
+
+// A function is a reusable block of code that performs a specific task.
+// function ek block of code hain jisko hum ek particular task perform krne 
+// keliye use krte hain.
+
+// Example of a function
+
+
+
+// function greet() {
+//     console.log("Hello, World!");
+// }
+// greet(); // Calling the function    
+// greet();
+// greet();
+
+// parameters
+// A parameter is a variable that is used to pass information into a function.
+// Ek parameter ek variable hota hai jisko hum function ke andar information pass
+// arguments
+
+
+// function hello(name,age){
+
+//  console.log("Hello",name, "your age is", age);
+// }
+
+// hello("rafiullah", 23); // Calling the function
+
+
+
+// function add(a,b){
+//     document.writeln(a+b);
+// }
+
+// add(25, 20); // Calling the function
+
