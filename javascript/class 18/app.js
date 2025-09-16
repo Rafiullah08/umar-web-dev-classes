3f3800e3de8af4c32fd86512751a62a9aad5a818
Rafi ullah <you@example.com  git config --global user.name Your>
2025-09-16 18:59:29 +0100

    dom start

---
+++ javascript/class 18/app.js
@@ -0,0 +1,14 @@
+alert()
+
+// This is a simple JavaScript alert to demonstrate the code structure
+function add(num1, num2,num3){
+
+
+    return num1 + num2+ num3;
+}
+
+let result= add(5, 10,13);
+console.log("The result is: " + result);
+
+
+
---
+++ javascript/class 19/app.js
@@ -0,0 +1,38 @@
+// alert("hello")
+
+// DOM = Document Object Model
+
+// window . ye global object hai
+
+// window
+// document
+// html
+// head
+// body
+// div
+// h1
+// p
+
+
+//node => node hamre object ko represent krta hai
+    // dom manipulation
+// document.getElementById() 
+// document.getElementsByClassName()
+// document.getElementsByTagName()
+// document.querySelector() => single element ko select krta hai
+// document.querySelectorAll() => multiple elements ko select krta hai
+
+// let para = document.getElementById("para");
+// console.log(para);
+
+// let para2 = document.getElementsByClassName("name");
+// console.log(para2);
+
+// let heading = document.getElementsByTagName("h2");
+// console.log(heading); //undefined
+
+let head2 = document.querySelector("head2")
+console.log(head2);
+
+
+
