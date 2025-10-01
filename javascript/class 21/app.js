39359a7f23844b0473b4940aa6994d16b22a0f9f
Rafi ullah <you@example.com  git config --global user.name Your>
2025-10-01 18:15:09 +0100

    settimeout and setinterval

--- javascript/class 21/app.js
+++ javascript/class 21/app.js
@@ -1,45 +1,38 @@
-// aler("hello")
+// // aler("hello")
 
-let todoPara = document.getElementById("todopara");
-let todoList = document.getElementById("todoList");
-let btnDeleteAll = document.getElementById("btnDeleteAll");
-// console.log(btnDeleteAll);
+// let todoPara = document.getElementById("todopara");
+// let todoList = document.getElementById("todoList");
+// let btnDeleteAll = document.getElementById("btnDeleteAll");
+// // console.log(btnDeleteAll);
 
 
 
-function addTodo() {
-   let todoValue   = todoPara.value;
-//    console.log(todoValue);
+// function addTodo() {
+//    let todoValue   = todoPara.value;
+// //    console.log(todoValue);
 
-  let todoData  = `<div class="todo mt-3">
+//   let todoData  = `<div class="todo mt-3">
   
-     <span class="todoText">${todoValue}</span>
-        <button class="btn btn-danger" >Delete</button>
-        <button class="btn btn-danger">Edit</button>
+//      <span class="todoText">${todoValue}</span>
+//         <button class="btn btn-danger" ">Delete</button>
+//         <button class="btn btn-danger">Edit</button>
 
-    </div>`
+//     </div>`
 
-todoList.innerHTML += todoData
-todoPara.value = "";
+// todoList.innerHTML += todoData
+// todoPara.value = "";
     
-}
+// }
 
- // Delete All  Function
-function deleteAll() {
-todoList.innerHTML = "";
+//  // Delete All  Function
+// function deleteAll() {
+// todoList.innerHTML = "";
     
-}
+// }
 
 
+// // Event Listener
+// btnDeleteAll.addEventListener("click", function(){
+//     deleteAll();
 
-
-
-
-
-
-
-// Event Listener
-btnDeleteAll.addEventListener("click", function(){
-    deleteAll();
-
-})
+// })
---
+++ javascript/class 22/app.js
@@ -0,0 +1,28 @@
+// setTimeout(function(){
+//     console.log("hello after 2 seconds")
+// }, 2000)
+
+// let btn = document.querySelector("button")
+
+//   let settime =setInterval(function(){
+//     console.log("hello after 2 seconds");
+  
+// },2000)
+
+// function stopInterval(){
+//  clearInterval(settime)
+// }
+
+// stopwatch, watch
+
+
+// global variables
+// local variables
+
+let count = 0;
+ 
+function stopInterval(){
+let b = 0;
+
+}
+
