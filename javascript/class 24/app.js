6849775c8c5ecf3b84553cdd583865bd024cb9d0
Rafi ullah <you@example.com  git config --global user.name Your>
2025-10-12 18:39:32 +0100

    object start

---
+++ javascript/class 24/app.js
@@ -0,0 +1,32 @@
+// alert("Hello, World!");
+// objects
+
+
+
+let array = ["asif", 2, 3, 4, 5]
+
+
+
+var obj = {
+    fname : "Rafi",
+    lname : "ullah",
+    age : 23,
+    fullName : function(){
+        return this.fname + " " + this.lname + "age" + this.age
+    },
+
+    hoobies: ["reading book", "traveling", 2,3,4]
+
+}
+
+// document.writeln(array, );
+// document.writeln(obj.fullName());
+
+// diff bw function and method
+
+// console.log(this.location);
+
+document.writeln(obj.hoobies)
+
+
+
