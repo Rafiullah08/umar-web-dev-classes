a7150e86446886e81234ad7459d83abda6de4652
Rafi ullah <you@example.com  git config --global user.name Your>
2025-10-18 18:52:44 +0100

    local storage and arrow function

---
+++ javascript/class 26/app.js
@@ -0,0 +1,89 @@
+// alert("hello")
+
+// function getdata(){
+//   let  name = document.getElementById("name").value
+//   let  email = document.getElementById("email").value
+//   let  phone = document.getElementById("phone").value
+
+
+// let stdObj = {
+//     stdName : name,
+//     stdEmial : email,
+//     stdPhone : phone 
+// } 
+
+// console.log(stdObj);
+
+// window.localStorage.setItem("stdName", name)
+// window.localStorage.setItem("stdemail", email)
+// window.localStorage.setItem("stdPhone", phone)
+
+// console.log("data save sucessfully");
+
+
+// }
+
+
+// function showStdData(){
+//    let stdName = window.localStorage.getItem("stdName")
+//    let stdEmail = window.localStorage.getItem("stdemail")
+//    let stdphone = window.localStorage.getItem("stdPhone")
+
+//    console.log(stdName, stdEmail, stdphone,"stdData");
+
+// }
+
+
+// localStorage.removeItem("stdObj")
+
+
+// javscript basic // before 2016  => ES5 
+// javascrpt advanced // after 2016 => ES6
+
+
+// variable ko initilazie krna
+// variable ke value reassinged krna
+// ek hi name se 2 viriable bnana
+
+// var   
+var a; 
+a = "rafi"
+
+var name = "abc"
+var name = "fgjhjhjj"
+
+// let 
+let std;
+std = "rafiullah"
+
+let num1 = 233
+// let num1 = 7887
+
+num1 = 34555
+// const
+
+
+function hello(){
+   console.log("hello");   
+}
+
+
+hello()
+
+let myfun = ()=>{
+    console.log("hello my arrow fun");
+    
+}
+
+myfun()
+
+
+let addnum = (a , b)=> {
+    return a + b
+       
+}
+
+
+
+
+console.log(addnum(23 , 34));
